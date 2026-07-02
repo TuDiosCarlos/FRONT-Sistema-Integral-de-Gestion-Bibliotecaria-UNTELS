18164f6f654c5e814b3f87a4222ae3b0a70d7831
cesar salazar <2213110208@untels.edu.pe>
2026-07-02 17:14:52 -0500

    TF11.1.1 implementacion notificacionservice

--- src/app/services/notificacionservice.ts
+++ src/app/services/notificacionservice.ts
@@ -1,4 +1,53 @@
-import { Service } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Service()
-export class Notificacionservice {}
+import { environment } from '../../environments/environment';
+import { Notificacion } from '../models/notificacion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificacionService {
+
+  private http = inject(HttpClient);
+
+  private url = `${environment.baseUrl}/api/notificaciones`;
+
+  constructor() { }
+
+  buscarPorEstudiante(
+    idEstudiante: number
+  ): Observable<Notificacion[]> {
+
+    return this.http.get<Notificacion[]>(
+      `${this.url}/estudiante/${idEstudiante}`
+    );
+
+  }
+
+  buscarPendientes(
+    idEstudiante: number
+  ): Observable<Notificacion[]> {
+
+    return this.http.get<Notificacion[]>(
+      `${this.url}/estudiante/${idEstudiante}/pendientes`
+    );
+
+  }
+
+  marcarLeidas(
+    idEstudiante: number
+  ): Observable<string> {
+
+    return this.http.put(
+      `${this.url}/marcar-leidas/${idEstudiante}`,
+      {},
+      {
+        responseType: 'text'
+      }
+    );
+
+  }
+
+}
