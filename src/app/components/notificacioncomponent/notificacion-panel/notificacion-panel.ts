c88b780a7b52f6b830c18e18547184d521768018
cesar salazar <2213110208@untels.edu.pe>
2026-07-02 17:18:39 -0500

    TF11.1.2 implementacion notificacion-panel

--- src/app/components/notificacioncomponent/notificacion-panel/notificacion-panel.ts
+++ src/app/components/notificacioncomponent/notificacion-panel/notificacion-panel.ts
@@ -1,9 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Notificacion } from '../../../models/notificacion';
+import { NotificacionService } from '../../../services/notificacionservice';
 
 @Component({
   selector: 'app-notificacion-panel',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
   templateUrl: './notificacion-panel.html',
-  styleUrl: './notificacion-panel.css',
+  styleUrl: './notificacion-panel.css'
 })
-export class NotificacionPanel {}
+export class NotificacionPanel implements OnInit {
+
+  private notificacionService = inject(NotificacionService);
+
+  listaNotificaciones: Notificacion[] = [];
+
+  cargando: boolean = false;
+
+  /*
+    Temporal:
+    posteriormente el id se obtendrá del usuario autenticado
+  */
+  idEstudiante: number = 1;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar(): void {
+
+    this.cargando = true;
+
+    this.notificacionService
+      .buscarPorEstudiante(this.idEstudiante)
+      .subscribe({
+        next: (data) => {
+
+          this.listaNotificaciones = data;
+
+          this.cargando = false;
+
+        },
+        error: (error) => {
+
+          console.error(error);
+
+          this.cargando = false;
+
+        }
+      });
+
+  }
+
+  marcarLeidas(): void {
+
+    const confirmar = confirm(
+      '¿Desea marcar todas las notificaciones como leídas?'
+    );
+
+    if (!confirmar) return;
+
+    this.notificacionService
+      .marcarLeidas(this.idEstudiante)
+      .subscribe({
+        next: (mensaje) => {
+
+          alert(mensaje);
+
+          this.listar();
+
+        },
+        error: (error) => {
+
+          console.error(error);
+
+          alert('No se pudieron actualizar las notificaciones');
+
+        }
+      });
+
+  }
+
+}
